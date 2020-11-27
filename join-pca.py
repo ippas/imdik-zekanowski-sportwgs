9a5b1003db080dd1fab527e078d96e616f8b6704
gosborcz <malgorzata.m.borczyk@gmail.com>
2020-11-27 17:35:51 +0100

    join pca

---
+++ join-pca.py
@@ -0,0 +1,18 @@
+#this script makes a large matrix table, runs PCA and
+
+import hail as hl
+
+mts = []
+
+for i in ['001', '002', '003', '004', '005', '006', '007', '008', '009',
+    '010', '011', '012', '013', '014', '015', '016', '017', '018', '019', '020']:
+
+    mts.append(hl.read_matrix_table('gs://hail-data/mts/part'+i+'-filtered-anno.mt'))
+
+mt = hl.MatrixTable.union_rows(*mts)
+mt.write('gs://hail-data/mts/sportsmen.mt')
+mt = hl.read_matrix_table('gs://hail-data/mts/sportsmen.mt')
+mt_sample = mt.sample_rows(0.0001)
+eigenvalues, pcs, _ = hl.hwe_normalized_pca(mt_sample.GT)
+mt_sample = mt_sample.annotate_cols(scores = pcs[mt_sample.s].scores)
+mt = mt.annotate_cols(scores = pcs[mt.s].scores)
