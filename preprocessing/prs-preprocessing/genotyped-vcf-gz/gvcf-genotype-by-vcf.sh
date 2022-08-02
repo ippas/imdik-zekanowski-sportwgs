35cd3d867c6a3e8009fd4f4839a708d6f02509f0
mateuszzieba <mateuszzieba97@gmail.com>
2022-08-02 15:16:12 +0200

    update genotyped-vcf-gz folder

--- preprocessing/prs-preprocessing/genotyped-vcf-gz/gvcf-genotype-by-vcf.sh
+++ preprocessing/prs-preprocessing/genotyped-vcf-gz/gvcf-genotype-by-vcf.sh
@@ -3,7 +3,7 @@
 #SBATCH -A plgsportwgs2
 #SBATCH -p plgrid
 #SBATCH -t 72:0:0
-#SBATCH --mem=2GB
+#SBATCH --mem=8GB
 #SBATCH --cpus-per-task=1
 #SBATCH -C localfs
 #SBATCH --output=/net/archive/groups/plggneuromol/matzieb/slurm-log/%j.out
@@ -12,12 +12,27 @@
 module load plgrid/tools/java11/11
 export TOOLS_DIR="/net/archive/groups/plggneuromol/tools/"
 
+
+while test $# -gt 0; do
+    case "$1" in
+        --inputs)
+            shift
+            inputs=$1
+            shift
+            ;;
+        *)
+            echo "$1 is not a recognized flag!"                 
+            break;
+            ;;
+    esac
+done 
+
 sg plggneuromol -c 'java \
         -Dconfig.file=preprocessing/prs-preprocessing/genotyped-vcf-gz/cromwell.conf \
         -Djava.io.tmpdir=$SCRATCH_LOCAL \
         -jar $TOOLS_DIR/cromwell run \
                 preprocessing/prs-preprocessing/genotyped-vcf-gz/gvcf-genotype-by-vcf.wdl \
-                --inputs preprocessing/prs-preprocessing/genotyped-vcf-gz/inputs-gvcf-genotype-by-vcf.json \
+                --inputs '"$inputs"' \
                 --options preprocessing/prs-preprocessing/genotyped-vcf-gz/options-gvcf-genotype-by-vcf.json'
 
 
